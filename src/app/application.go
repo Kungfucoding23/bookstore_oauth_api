fdc72e06292a3b4531558635154600e72e4f5485
Alejandro <alesantin012@hotmail.com>
2021-03-19 14:50:19 -0300

    cassandra integration

--- src/app/application.go
+++ src/app/application.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/Kungfucoding23/bookstore_oauth_api/src/clients/cassandra"
 	"github.com/Kungfucoding23/bookstore_oauth_api/src/domain/access_token"
 	"github.com/Kungfucoding23/bookstore_oauth_api/src/http"
 	"github.com/Kungfucoding23/bookstore_oauth_api/src/repository/db"
@@ -12,6 +13,11 @@ var (
 )
 
 func StartApp() {
+	session, dbErr := cassandra.GetSession()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	session.Close()
 	atService := access_token.NewService(db.NewRepository())
 	atHandler := http.NewHandler(atService)
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetByID)
--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/Kungfucoding23/bookstore_oauth_api/src/clients/cassandra"
 	"github.com/Kungfucoding23/bookstore_oauth_api/src/domain/access_token"
 	"github.com/Kungfucoding23/bookstore_oauth_api/src/utils/errors"
 )
@@ -16,5 +17,11 @@ type DBRepository interface {
 type dbRepository struct{}
 
 func (r *dbRepository) GetByID(string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	//TODO: implement get access token from cassandra db
 	return nil, errors.NewInternalServerError("database connection not implemented yet")
 }
